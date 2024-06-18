0de517ae21336d2ebe4ee83fdbae479637e6eade
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-06-18 14:57:55 +0330

    create navbar item success

--- src/components/module/Navbar/Navbar.jsx
+++ src/components/module/Navbar/Navbar.jsx
@@ -1,5 +1,8 @@
 import './Navbar.css';
-import { FaSearch , FaPlus } from "react-icons/fa";
+import { FaSearch , FaPlus , FaMoon  } from "react-icons/fa";
+import { FaMessage } from "react-icons/fa6";
+import { MdSettings } from "react-icons/md";
+
 
 function Navbar() {
   return (
@@ -24,7 +27,28 @@ function Navbar() {
                     </span>
                 </div>
             </div>
-            <div className="navbar_item">this is item</div>
+            <div className="navbar_item">
+                <div className="navbar_item_wrapper">
+                <div className="navbar_theme">
+                    <span className="navbar_theme_icon">
+                        <FaMoon/>
+                    </span>
+                </div>
+                <div className="navbar_message">
+                    <span className="navbar_message_icon">
+                        <FaMessage/>
+                    </span>
+                </div>
+                <div className="navbar_setting">
+                    <span className="navbar_setting_icon">
+                        <MdSettings/>
+                    </span>
+                </div>
+                <div className="navbar_profile">
+                    <img src="/assets/images/pic1.jpg" alt="profile image" className="navbar_profile_img" />
+                </div>
+                </div>
+            </div>
         </div>
     </section>
   )
