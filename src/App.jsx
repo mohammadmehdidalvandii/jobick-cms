e59023d99aeee9f2cb1e3ffe0e71aac881a6ee1c
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-07-01 15:26:25 +0330

    create page section and app-site

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
+import { useRoutes } from "react-router-dom";
 import Navbar from "./components/module/Navbar/Navbar"
 import Sidebar from "./components/module/Sidebar/Sidebar"
+import routes from './routes';
+
+
 
 function App() {
+  const router = useRoutes(routes)
   return (
     <>
-    <div className="app_Wrapper">
       <div className="app_navbar">
          <Navbar/>
       </div>
+    <div className="app_Wrapper">
       <div className="app_side">
           <Sidebar/>
+      </div>
+      <div className="app_site">
+      {router}
       </div>
     </div>
     </>
